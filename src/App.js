fe88bede7612ff60154d8108efd8079b5374c052
Rodrigo Almeida de Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2020-11-12 00:50:39 -0300

    exibir o produto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Routes from './routes';
 
 import './style.css';
 import Header from './components/Header';
 import Main from './pages/main';
+import { Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
 import './style.css';
 
 export default class Main extends Component {
@@ -31,6 +33,7 @@ export default class Main extends Component {
     const pageNumber = page - 1;
     this.loadProducts(pageNumber);
   }
+
   nextPage = () => {
     const { page, productInfo } = this.state;
 
@@ -47,11 +50,11 @@ export default class Main extends Component {
       <div className="product-list">
         {products.map(product => {
           return (
-            <article key={product.id}>
+            <article key={product._id}>
               <strong>{product.title}</strong>
               <p>{product.description}</p>
 
-              <a href="">Acessar</a>
+              <Link to={`/products/${product._id}`}>Acessar</Link>
             </article>
           );
         })}
